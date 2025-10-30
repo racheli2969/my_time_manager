1e68aab485df7e420dfa5b78b9e98c7ed4952025
rachel pokroy <racheli2969@gmail.com>
2025-10-31 01:53:26 +0200

    server env fix

--- server/server.js
+++ server/server.js
@@ -24,7 +24,8 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
+  // Load from .env file
+  origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ['http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
